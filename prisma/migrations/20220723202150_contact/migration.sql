2e578251d2509ee85500c422d6f65f972f4ec936
devrayat000 <2010050@me.buet.ac.bd>
2022-07-24 02:25:02 +0600

    created prisma models

---
+++ prisma/migrations/20220723202150_contact/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - Changed the type of `id` on the `User` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ALTER COLUMN "name" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "email" SET DATA TYPE VARCHAR(255),
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "phone" CHAR(10),
+    "email" VARCHAR(255),
+    "address" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
